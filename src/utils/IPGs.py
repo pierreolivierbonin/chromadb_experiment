3de8078992bf36c5eef2a5998e0d63614113ce81
Pierre-Olivier Bonin <pierre-olivier.bonin@hotmail.com>
2025-01-10 15:14:11 -0500

    latest changes

--- src/utils/IPGs.py
+++ src/utils/IPGs.py
@@ -1,7 +1,20 @@
+from io import StringIO
+
 from dataclasses import dataclass
+from pandas import read_html
+import requests
 
 @dataclass
-class GenericIPG():
+class HTMLTablestoDataframes:
+    url:str = None
+
+    def __post_init__(self):
+        html_content = requests.get(self.url)
+        self.df_list = pd.read_html(StringIO(html_content.text))
+
+
+@dataclass
+class GenericIPG:
     """A general class to hold data from Interpretations, Policies, and Guidelines (IPGs) 
     found at https://www.canada.ca/en/employment-social-development/programs/laws-regulations/labour/interpretations-policies.html#st"""
 
@@ -11,19 +24,20 @@ class GenericIPG():
     text:str = "<placeholder>"
 
     def fetch_text_from_hyperlinks(self):
+        """Requires having assigned hyperlink beforehand."""
         specific_IPG = requests.get(self.hyperlink)
 
         soup = BeautifulSoup(specific_IPG.text, features="html.parser")
 
         for i in soup(["script", "style"]):
             i.extract()
-        text = soup.get_text()
-        text = re.sub(r'(\n\s*)+\n+', '\n\n', text)
+            text = soup.get_text()
+            text = re.sub(r'(\n\s*)+\n+', '\n\n', text)
 
-        start = 'Interpretations, Policies and Guidelines (IPGs)'
-        end = 'About this site'
-        cleaned_text = text[text.find(start)+len(start):text.rfind(end)]
-        self.text = cleaned_text
+            start = 'Interpretations, Policies and Guidelines (IPGs)'
+            end = 'About this site'
+            cleaned_text = text[text.find(start)+len(start):text.rfind(end)]
+            self.text = cleaned_text
 
         return
 
@@ -37,30 +51,45 @@ if __name__ == "__main__":
     import re
     import requests
 
+    # load only the first HTML table for LS IPGs (Employment standards) as an example
     url = "https://www.canada.ca/en/employment-social-development/programs/laws-regulations/labour/interpretations-policies.html"
-    r = requests.get(url)
-    df_list = pd.read_html(StringIO(r.text))
-    IPGs = df_list[0].values # this is the first dataframed table (i.e. the Labour Standards IPGs (employment standards)
+    IPGs_tables = HTMLTablestoDataframes(url=url)
+    LS_IPGs = IPGs_tables.df_list[0].values
 
-    preprocessed_IPGs = [GenericIPG(id=i[1], title=i[0]) for i in IPGs]
+    # organize and check
+    preprocessed_IPGs = [GenericIPG(id=i[1], title=i[0]) for i in LS_IPGs]
     print([(i.id, i.title) for i in preprocessed_IPGs])
 
-    # fetch html object
+    # fetch html objects at given hyperlink
     IPGs_main_page = requests.get("https://www.canada.ca/en/employment-social-development/programs/laws-regulations/labour/interpretations-policies.html#st")
 
+    # extract the table and hrefs
     ls_ipgs_table = BeautifulSoup(IPGs_main_page.text, features="lxml")
     ls_ipgs_table_hrefs = ls_ipgs_table.find_all(href=True)
 
-    result = re.search('<a href=(.*)</a>', str(ls_ipgs_table_hrefs))
-    result = [i for i in result.group(0).split("</a>")]
+    result = re.search('<a href=(.*)</a>', str(ls_ipgs_table_hrefs)) # extracts all the hyperlink HTML objects (hrefs)
+    result = [" ".join(i.lower().split()) for i in result.group(0).split("</a>")]      # lowercase + whitespace removal to avoid matching issues
 
-    hrefs = []
+    # add the hyperlinks that were embedded in each LS IPG to the class instance
     for i in result:
         for j in preprocessed_IPGs:
-            href = re.search(f'<a href="(.*)">{j.title}', i)
+            href = re.search(f'<a href="(.*)">{" ".join(j.title.lower().split())}', i) # lowercase + whitespace removal to avoid matching issues
             if href is not None:
-                # print("canada.ca" + href.group(1))
-                j.hyperlink = "canada.ca" + href.group(1)
+                j.hyperlink = "https://www.canada.ca" + href.group(1)
 
+    # check if everything is looking good
     for i in preprocessed_IPGs:
         print([i.id, i.title, i.hyperlink])
+
+    # final step: add the text found at the embedded hyperlink --> longer operation as it has to request each page in a loop
+    for i in preprocessed_IPGs:
+        i.fetch_text_from_hyperlinks()
+    
+    # check if everything is looking good
+    for i in preprocessed_IPGs[:5]:
+        print([i.id, i.title, i.hyperlink, i.text[:100]])
+
+    # save to dataframe, then save file for reusability (preparing for transfer into ChromaDB)
+    cols = ["Number", "Name", "Text"]
+    df = pd.DataFrame(preprocessed_IPGs)
+    df.to_csv("IPGs_Labour_standards.csv", index=False)
